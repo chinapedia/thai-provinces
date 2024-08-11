0b96df01c4dfefe9c7021835a6a5c81f8c97da26
liruqi <liruqi@gmail.com>
2024-08-11 13:20:15 +0700

    脚本更新

--- readme.py
+++ readme.py
@@ -13,7 +13,7 @@ with open(input_file, 'r', encoding='utf-8-sig') as f:
     provinces = json.load(f)
 
 # 创建 GFM 格式的表格
-table = "# Thailand Administrative Division Database\n\n| PROVINCE_ID | English Name | Thai Name | Chinese Name |\n"
+table = "# 泰国行政区划数据库\n\n| 编号 | 英文名 | 泰语名 | 中文名 |\n"
 table += "|-------------|--------------|-----------|--------------|\n"
 
 # 遍历每个省份
